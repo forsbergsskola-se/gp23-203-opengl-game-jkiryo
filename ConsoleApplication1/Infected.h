e5f40d5f4dd6b38a85072773d7c161e4c253cf1e
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-03-10 13:19:52 +0100

    added comments

--- ConsoleApplication1/Infected.h
+++ ConsoleApplication1/Infected.h
@@ -4,35 +4,38 @@
 
 using namespace std;
 
-class Infected : public Unit {
+class Infected : public Unit //Inheriting from the unit class
+{
 
 public:
-    Infected() : Unit("Infected", 2), revived(false) {} //parameter less constructors
+    Infected() : Unit("Infected", 2), revived(false) {}
 
+    //overriding the takeDamage method
     virtual void takeDamage(int damage) override 
     {
 
         Unit::takeDamage(damage);
 
+        //Checking if the infected enemy is dead or not
         if (is_dead() && !Risen) 
         {
 
-            Risen();
+            Risen(); //The infected rises from th dead
         }
     }
 
 private:
     bool revived;
 
-    void Risen() 
+    void Risen() //Infected rises
 {
         cout << "Infected has risen from the dead as Zombie with 1 Health.\n";
 
-        name_ = "Zombie";
+        name_ = "Zombie"; //changes infected to zombie
 
-        health_ = 1;
+        health_ = 1; //Sets the zombies health to 1
 
-        revived = true;
+        revived = true; // Marked as revived
     }
 };
 
