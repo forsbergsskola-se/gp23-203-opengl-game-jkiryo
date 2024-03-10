93dc2b341585930a5e5e28ea310187547112bd9d
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-03-10 02:54:03 +0100

    Fixed both skeleton header and unit header

--- ConsoleApplication1/Skeleton.h
+++ ConsoleApplication1/Skeleton.h
@@ -1,13 +1,14 @@
 #pragma once
-#include "Unit.h"
-class Skeleton
+#include "unit.h"
+
+class Skeleton : public Unit
 {
-	class skeleton : Unit
+public:
+    Skeleton() : Unit("Skeleton", 4) {}
 
-	{
-	public:
-
-
-	};
+    virtual void takeDamage(int damage) override
+    {
+        cout << "The attack is very effective!\n";
+        Unit::takeDamage(damage * 2);
+    }
 };
-
