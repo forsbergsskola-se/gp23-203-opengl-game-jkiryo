0db7001fdc5fdc3cf97881a443431c61a5376cde
Jakobkiryo <143487429+Jakobkiryo@users.noreply.github.com>
2024-03-10 02:58:41 +0100

    Fixed unit header and put them the right code in the right header

--- ConsoleApplication1/Unit.h
+++ ConsoleApplication1/Unit.h
@@ -3,47 +3,49 @@
 #include <iostream>
 #include <Windows.h>
 
-using namespace std;
 class Unit
 {
-    class unit
+public:
+
+    explicit Unit(const char* name, const int health) : name_{ name }
     {
+        health_ = clamp(health, 0, 100);
 
-    public:
-        explicit unit(const char* name, const int health) : name_{ name }
-        {
-            health_ = clamp(health, 0, 100);
+        cout << name << " \033[1;32mspawned\033[0m with \033[1;34m" << health << " Health\033[0m.\n";
+    }
 
-            cout << name << " \033[1;32mspawned\033[0m with \033[1;34m" << health << " Health\033[0m.\n";
-        }
+    // Virtual destr
+    virtual ~Unit()
+    {
+        cout << name_ << " \033[1;31mdied\033[0m.\n";
+    }
 
-        ~unit()
-        {
-            cout << name_ << " \033[1;31mdied\033[0m.\n";
-        }
+    void attack(Unit& other)
+    {
+        cout << name_ << " \033[1;31mattacks\033[0m " << other.name_ << ".\n";
+        other.takeDamage(1);
+    }
 
-        void attack(unit& other)
-        {
-            cout << name_ << " \033[1;31mattacks\033[0m " << other.name_ << ".\n";
+    bool is_dead() const { return health_ <= 0; }
 
-            other.set_health(other.get_health() - 1);
-        }
+    int get_health() const { return health_; }
 
-        bool is_dead() const { return health_ <= 0; }
+    void set_health(const int value)
+    {
+        health_ = clamp(value, 0, 100);
 
-        int get_health() const { return health_; }
+        cout << name_ << " now has \033[1;34m" << health_ << " Health\033[0m.\n";
+    }
 
-        void set_health(const int value)
-        {
-            health_ = clamp(value, 0, 100);
+    virtual void takeDamage(int damage)
+    {
+        health_ -= damage;
+    }
 
-            cout << name_ << " now has \033[1;34m" << health_ << " Health\033[0m.\n";
-        }
-
-    private:
-        const char* name_;
-
-        int health_;
-    };
+protected:
+    const char* name_;
+    int health_;
 };
+
+
 
